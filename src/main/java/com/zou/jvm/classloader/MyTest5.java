b05da1f77dbfea3ba2aabfc9e9a02f7e241b8a21
zoulvzhou <940933073@qq.com>
2018-02-22 12:35:12 +0800

    接口的初始化规则

---
+++ src/main/java/com/zou/jvm/classloader/MyTest5.java
@@ -0,0 +1,27 @@
+package com.zou.jvm.classloader;
+
+/**
+ * 当一个接口初始化时，并不要求其父接口都完成了初始化。
+ *
+ * 只有在真正使用父接口的时候（如引用接口中所定义的常量时），才会初始化。
+ */
+
+public class MyTest5 {
+
+    public static void main(String[] args) {
+
+        System.out.println(MyChild5.b);
+    }
+
+}
+
+interface MyParent5 {
+
+    public static int a = 5;
+
+}
+
+interface MyChild5 extends MyParent5 {
+
+    public static int b = 6;
+}
