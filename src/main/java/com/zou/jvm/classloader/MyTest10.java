516dce146a98f65566bab445907ff27b0c641995
zoulvzhou <940933073@qq.com>
2018-03-04 01:09:24 +0800

    类加载器实例4

---
+++ src/main/java/com/zou/jvm/classloader/MyTest10.java
@@ -0,0 +1,55 @@
+package com.zou.jvm.classloader;
+
+
+class Parent2 {
+
+    static int a = 3;
+
+    static  {
+        System.out.println("Parent2  static  block");
+    }
+}
+
+
+class Child2 extends Parent2 {
+
+    static int b = 4;
+
+    static {
+        System.out.println("Child2  static  block");
+    }
+}
+
+/**
+ * @author zlz
+ */
+public class MyTest10 {
+
+    static {
+        System.out.println("MyTest10   static  block");
+    }
+
+    public static void main(String[] args) {
+
+        Parent2 parent2;
+
+        System.out.println("-------");
+
+        //对类的主动使用
+        parent2 = new Parent2();
+
+        System.out.println("---------");
+
+        System.out.println(parent2.a);
+
+        System.out.println("-----------");
+
+        System.out.println(Child2.b);
+
+
+
+    }
+
+
+
+}
