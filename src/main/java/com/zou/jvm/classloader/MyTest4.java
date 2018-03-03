6bf023fb60aa69ebaf99ab0703f91c3d05a6fd73
zoulvzhou <940933073@qq.com>
2018-03-03 23:38:51 +0800

    增加阿里编程规约扫描

--- src/main/java/com/zou/jvm/classloader/MyTest4.java
+++ src/main/java/com/zou/jvm/classloader/MyTest4.java
@@ -2,6 +2,8 @@ package com.zou.jvm.classloader;
 
 
 /**
+ * @author zlz
+ *
  * 对于数组实例来说，其类型是由JVM运行期间动态生成的，表示为： class [Lcom.zou.jvm.classloader.MyParent4
  * 动态生成的类型，其父类型就是Object。
  * 对于数组来说，Javadoc经常将构成数组的元素为Compoent，实际上就是一个数组降低一个维度。
@@ -18,21 +20,28 @@ public class MyTest4 {
 
     public static void main(String[] args) {
 
-       // MyParent4 myParent4 = new MyParent4(); //输出  MyParent4  static  block
+        //输出  MyParent4  static  block
+       // MyParent4 myParent4 = new MyParent4();
 
-        MyParent4[] myParent4s =new MyParent4[1]; //无输出
+        //无输出
+        MyParent4[] myParent4s =new MyParent4[1];
 
-        System.out.println(myParent4s.getClass()); //  class [Lcom.zou.jvm.classloader.MyParent4  . Java虚拟机在运行时生成.
+        //  class [Lcom.zou.jvm.classloader.MyParent4  . Java虚拟机在运行时生成.
+        System.out.println(myParent4s.getClass());
 
         //二维数组
         MyParent4[][]  myParent4s1 = new MyParent4[1][1];
-        System.out.println(myParent4s1.getClass()); //  class [[Lcom.zou.jvm.classloader.MyParent4;
+
+        //  class [[Lcom.zou.jvm.classloader.MyParent4;
+        System.out.println(myParent4s1.getClass());
 
         System.out.println("*******************");
 
+        //class [I
         int[]  ints = new int[1];
-        System.out.println(ints.getClass());  //class [I
-        System.out.println(ints.getClass().getSuperclass()); // class java.lang.Object
+        System.out.println(ints.getClass());
+        // class java.lang.Object
+        System.out.println(ints.getClass().getSuperclass());
     }
 
 }
--- src/main/java/com/zou/jvm/classloader/MyTest5.java
+++ src/main/java/com/zou/jvm/classloader/MyTest5.java
@@ -1,8 +1,10 @@
 package com.zou.jvm.classloader;
 
 /**
- * 当一个接口初始化时，并不要求其父接口都完成了初始化。
  *
+ * @author zlz
+ * 当一个接口初始化时，并不要求其父接口都完成了初始化。
+ * <p>
  * 只有在真正使用父接口的时候（如引用接口中所定义的常量时），才会初始化。
  */
 
@@ -10,18 +12,18 @@ public class MyTest5 {
 
     public static void main(String[] args) {
 
-        System.out.println(MyChild5.b);
+        System.out.println(MyChild5.B);
     }
 
 }
 
 interface MyParent5 {
 
-    public static int a = 5;
+    public static int A = 5;
 
 }
 
 interface MyChild5 extends MyParent5 {
 
-    public static int b = 6;
+    public static int B = 6;
 }
